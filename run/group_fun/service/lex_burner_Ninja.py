ecdc08a513834dd922ce772f8dddef63ebbb6f90
ColdWindScholar <3590361911@qq.com>
2025-06-27 00:37:12 +0800

    Revert "Fix An error"
    This reverts commit e6586bd1

--- run/group_fun/service/lex_burner_Ninja.py
+++ run/group_fun/service/lex_burner_Ninja.py
@@ -4,6 +4,10 @@ import httpx
 
 
 class Lexburner_Ninja:
+    """
+    类名不要瞎几把改。
+    改完用查找用法或者跑一遍测试。
+    """
     def __init__(self):
         self.name = "Lexburner_Ninja"
 
@@ -21,7 +25,7 @@ class Lexburner_Ninja:
                 return {"error": "No jutsus found for the search term"}
 
             first_jutsu = jutsus[0]
-            # print(first_jutsu)
+            #print(first_jutsu)
             return {
                 "title": first_jutsu.get("name", "未找到标题"),
                 "description": first_jutsu.get("description", "未找到描述"),
@@ -39,11 +43,11 @@ class Lexburner_Ninja:
             url = "https://wsfrs.com/api/jutsus?limit=1&sortBy=random"
             response = await client.get(url)
 
+
             data = response.json()
             jutsus = data.get("jutsus", [])
 
             return jutsus[0]
-
 
 if __name__ == '__main__':
     ninja = Lexburner_Ninja()
