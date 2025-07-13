2cff4a49890dfd4569b3d11cfd22bfe2d9e5cdee
avilliai <1840094972@qq.com>
2025-07-13 14:05:27 +0800

    菜单！

--- run/basic_plugin/basic_plugin.py
+++ run/basic_plugin/basic_plugin.py
@@ -166,7 +166,7 @@ def main(bot, config):
         if event.pure_text in ["帮助", "菜单", "/help", "/menu"]:
 
             file_lists = ['help_menu_page1.png', 'help_menu_page2.png', 'help_menu_page3.png', 'help_menu_page4.png']
-            node_list = []
+            node_list = [Node(content=[Text("项目文档：https://eridanus.netlify.app/\n项目地址：https://github.com/AOrbitron/Eridanus")])]
             for file_name in file_lists:
                 node_list.append(Node(content=[Image(file=os.path.join('data/pictures/cache', file_name))]))
             await bot.send(event, node_list)
