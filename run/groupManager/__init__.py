ecef91451e14268e3c49c8bb30b469486e9f99f7
avilliai <1840094972@qq.com>
2025-07-29 23:27:21 +0800

    诡异bug。没办法，回调吧。

--- run/groupManager/__init__.py
+++ run/groupManager/__init__.py
@@ -1,5 +1,9 @@
 plugin_description="群管理"
-
+"""
+各个入口文件
+"""
+from framework_common.framework_util.main_func_detector import load_main_functions
+entrance_func=load_main_functions(__file__)
 
 dynamic_imports={
     "run.groupManager.self_Manager": [
