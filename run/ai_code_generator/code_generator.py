d33e8ccd4fdebf9bc5023eab07ea6e1c947ccc68
avilliai <1840094972@qq.com>
2025-08-03 23:43:56 +0800

    严重漏洞

--- run/ai_code_generator/code_generator.py
+++ run/ai_code_generator/code_generator.py
@@ -1,5 +1,6 @@
 from developTools.event.events import GroupMessageEvent
 from developTools.message.message_components import Text
+from framework_common.database_util.User import get_user
 from framework_common.framework_util.websocket_fix import ExtendBot
 from framework_common.framework_util.yamlLoader import YAMLManager
 from run.ai_code_generator.service.AiPluginGenerator import code_generate
@@ -9,7 +10,8 @@ def main(bot: ExtendBot,config: YAMLManager):
     @bot.on(GroupMessageEvent)
     async def _(event: GroupMessageEvent):
         if event.pure_text.startswith(config.ai_code_generator.ai_coder["prefix"]):
-            if event.sender.user_id>config.ai_code_generator.ai_coder["code_generation_permission_need"]:
+            user_info = await get_user(event.user_id)
+            if user_info.permission>config.ai_code_generator.ai_coder["code_generation_permission_need"]:
                 prompt=event.pure_text.replace(config.ai_code_generator.ai_coder["prefix"],"").strip()
                 bot.logger.info(f"AI插件生成器收到需求:{prompt}")
                 r=await code_generate(config,prompt,event.user_id)
