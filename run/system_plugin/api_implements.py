6e6b9a732d4e121a70729efdbe213782db397fc4
avilliai <1840094972@qq.com>
2025-08-10 10:02:37 +0800

    漏的东西

--- run/system_plugin/api_implements.py
+++ run/system_plugin/api_implements.py
@@ -156,6 +156,7 @@ def main(bot, config):
                 bot.logger.info(text)
                 # print(text)
                 event.message_id=114514
+                event.sender.user_id=event.user_id
                 if config.system_plugin.config['api_implements']['nudge']['is_Reply_with_meme']:
                     if random.randint(1, 100) < config.system_plugin.config['api_implements']['nudge'][
                         'Reply_with_meme_probability']:
