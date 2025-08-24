88bf591652912c6964220890848632013df0b496
avilliai <1840094972@qq.com>
2025-08-24 18:05:31 +0800

    用户数据读取

--- run/groupManager/word_cloud.py
+++ run/groupManager/word_cloud.py
@@ -20,6 +20,7 @@ def main(bot,config):
             """
             增加一个日期提示
             """
+            await asyncio.sleep(3)
             current_datetime = datetime.now()
             formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
             event.processed_message.insert(0, {"text": f"(系统提示，当前时间为{formatted_datetime})"})
