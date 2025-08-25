f1a3c9be555f26e8223529c4ad90b9a67496e4a1
avilliai <1840094972@qq.com>
2025-08-25 10:32:46 +0800

    修改读取方式

--- run/anime_game_service/SteamSnooping.py
+++ run/anime_game_service/SteamSnooping.py
@@ -23,11 +23,10 @@ def main(bot, config):
             if config.anime_game_service.config['steamsnooping']['is_snooping']:
                 bot.logger.info(f"bot开始视奸群友的Steam啦！")
                 monitor_activated = True
-                loop = asyncio.get_event_loop()
-                loop.run_in_executor(
-                    None,
+                asyncio.to_thread(
                     lambda: asyncio.run(steamsnoopall(bot, config, db, steam_api_key))
                 )
+
 
     #绑定一个steamid
     @bot.on(GroupMessageEvent)
