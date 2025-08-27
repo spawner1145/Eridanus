e08e4edd3e90c45fed70c6f0c152302032ae966f
yubhu49-dotcom <yubhu49@gmail.com>
2025-08-27 16:18:57 +0800

    演示用

--- run/test/test.py
+++ run/test/test.py
@@ -1,4 +1,7 @@
+import requests
+
 from developTools.event.events import GroupMessageEvent
+from developTools.message.message_components import Image
 from framework_common.framework_util.websocket_fix import ExtendBot
 from framework_common.framework_util.yamlLoader import YAMLManager
 
@@ -7,4 +10,8 @@ def main(bot: ExtendBot,config: YAMLManager):
     @bot.on(GroupMessageEvent)
     async def handle_group_message(event: GroupMessageEvent):
         if event.pure_text=="测试244":
+            r=requests.get('https://api.xingzhige.com/API/Calendar/')
+            with open("123.png", "wb") as f:
+                f.write(r.content)
+            await bot.send(event,Image(file="123.png"))
             await bot.send(event,"Hello World!")
