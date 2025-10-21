8a61aa381fa9d330cdc977a80943923b4f2962e9
avilliai <1840094972@qq.com>
2025-10-21 14:16:17 +0800

    菜单更新

--- tool.py
+++ tool.py
@@ -270,7 +270,7 @@ def merge_dicts(old, new):
             merge_dicts(v, new[k])
         # 如果值是列表，且新旧值都是列表，则合并并去重
         elif isinstance(v, list) and k in new and isinstance(new[k], list):
-            if k in ["api_keys","sdUrl","其他默认绘图参数","card_index","steam_api_key","nano_banana_key"]:  # 特殊处理, 保留旧值
+            if k in ["api_keys","sdUrl","其他默认绘图参数","card_index","steam_api_key","nano_banana_key","自定义入群欢迎"]:  # 特殊处理, 保留旧值
                 logger.info(f"覆盖列表 key: {k}")
                 new[k] = v  # 使用旧的列表覆盖新的列表
             elif k.startswith("page"):
