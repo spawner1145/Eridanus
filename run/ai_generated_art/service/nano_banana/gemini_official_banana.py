8fdfee466fbb54029ac19c3ba3987f1997708961
spawner <spawner6@outlook.com>
2026-01-12 21:58:21 +0800

    Add tools configuration to generation settings

--- run/ai_generated_art/service/nano_banana/gemini_official_banana.py
+++ run/ai_generated_art/service/nano_banana/gemini_official_banana.py
@@ -31,8 +31,9 @@ async def call_gemini_api(contents, config) -> Dict[str, Any]:
             {'category': 'HARM_CATEGORY_HARASSMENT', "threshold": "BLOCK_None"},
             {'category': 'HARM_CATEGORY_DANGEROUS_CONTENT', "threshold": "BLOCK_None"}
         ],
-         "generationConfig":
-            {"temperature": config.ai_generated_art.config["ai绘画"]["nano_banana_config"]["temperature"],"responseModalities": ['Image'], "imageConfig": {"imageSize": "2K"}}
+        "tools": [{"google_search": {}}],
+        "generationConfig":
+            {"temperature": config.ai_generated_art.config["ai绘画"]["nano_banana_config"]["temperature"],"responseModalities": ["IMAGE"], "imageConfig": {"imageSize": "2K"}}
     }
     global api_key_selector
     api_keys = config.ai_generated_art.config["ai绘画"]["nano_banana_key"]
