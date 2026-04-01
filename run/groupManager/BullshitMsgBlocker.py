83f975a09cf924b00ddb7c829a1f46694be5e731
avilliai <1840094972@qq.com>
2026-04-01 19:05:44 +0800

    BullshitMsgBlocker.py增加白名单权限

--- run/groupManager/BullshitMsgBlocker.py
+++ run/groupManager/BullshitMsgBlocker.py
@@ -1,5 +1,6 @@
 from collections import deque, Counter
 from developTools.event.events import GroupMessageEvent
+from framework_common.database_util.User import get_user
 from framework_common.framework_util.websocket_fix import ExtendBot
 from framework_common.framework_util.yamlLoader import YAMLManager
 
@@ -32,6 +33,11 @@ def main(bot: ExtendBot, config: YAMLManager):
 
         # 判断是否刷屏
         if count >= config.groupManager.config["BullshitMsgBlocker"]["每人最大发言数量"]:
+            user_info = await get_user(event.user_id)
+            if user_info.permission >= config.system_plugin.config["BullshitMsgBlocker"]["白名单所需权限等级"]:
+                bot.logger.info(f"BullshitmsgBlocker： 用户{event.user_id} 权限满足白名单条件，不禁言")
+                group_message_queue[group_id].clear()
+                return
             await bot.mute(group_id, top_user, config.groupManager.config["BullshitMsgBlocker"]["禁言时长"])
             bot.logger.info(f"用户 {top_user} 因短时间内连续发送消息过多，已被禁言。")
             await bot.send(event, config.groupManager.config["BullshitMsgBlocker"]["嘲讽"])
