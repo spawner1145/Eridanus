08f1d0806da3b64980d955c66bfcfd5593bdcbc3
Alpherg <99066610+avilliai@users.noreply.github.com>
2026-04-14 10:08:50 +0800

    Update antinsfw.py

--- run/ai_llm/antinsfw.py
+++ run/ai_llm/antinsfw.py
@@ -129,7 +129,7 @@ def main(bot, config):
                 )
                 if send_gay_audio:
                     try:
-                        await bot.send(event,Record(file="data/audio/gay_audio/"+random.choice(os.listdir("data/audio/gay_audio"))))
+                        await bot.send(event,Record(file="data/voice/gay_audio/"+random.choice(os.listdir("data/voice/gay_audio"))))
                     except Exception as e:
                         bot.logger.error(e)
                         bot.logger.error("请在data/audio/gay_audio放入恶心人的音频")
