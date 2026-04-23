e9211ece931e84e00b1f5f9e0d4da33e6f68e732
Alpherg <99066610+avilliai@users.noreply.github.com>
2026-04-24 02:42:50 +0800

    Change command prefix from '/gi' to '/gi2'

--- run/ai_generated_art/gptimage2.py
+++ run/ai_generated_art/gptimage2.py
@@ -8,7 +8,7 @@ from run.ai_generated_art.function_collection import gptimage2_text2img, image_e
 def main(bot: ExtendBot,config: YAMLManager):
     @bot.on(GroupMessageEvent)
     async def handle_group_message(event: GroupMessageEvent):
-        if event.pure_text.startswith("/gi"):
+        if event.pure_text.startswith("/gi2"):
             prompt = event.pure_text[len("/gi2"):].strip()
 
             await gptimage2_text2img(bot,event,config,prompt)
