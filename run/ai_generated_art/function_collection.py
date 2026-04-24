4ad9a0e5c9704b1b529f30e85f08f68ef45c3047
avilliai <1840094972@qq.com>
2026-04-24 14:36:03 +0800

    gptimage2修复

--- run/ai_generated_art/function_collection.py
+++ run/ai_generated_art/function_collection.py
@@ -1,4 +1,5 @@
 import base64
+import os
 
 import httpx
 
@@ -13,24 +14,37 @@ async def image_edit(bot,event,config,prompt,image_url):
     if user.permission<permission_need:
         return
     image_path=await download_img(image_url)
-    aim_url="http://apollodorus.xyz:8080/v1/images/generations"
-    with open(image_path, "rb") as f:
-        base64_str = base64.b64encode(f.read()).decode("utf-8")
+    aim_url="http://apollodorus.xyz:8080/v1/images/edits"
 
-    image_input = f"data:image/png;base64,{base64_str}"
+    file_objects = []
+    f = open(image_path, "rb")
+    # 格式: (字段名, (文件名, 文件流, MIME类型))
+    # 使用 "images" 作为字段名，对应你 API 支持的多图上传
+    file_objects.append(("images", (os.path.basename(image_path), f, "image/png")))
+
+
     apikey=config.ai_generated_art.config["gptimage2"]["apikey"]
     headers = {"Authorization": f"Bearer {apikey}"}
-    payload = {
+
+    data = {
         "prompt": prompt,
-        "image": image_input,
-        "aspect_ratio": "1:1",
-        "quality": "medium"
+        "size": "1024x1024"
     }
-    async with httpx.AsyncClient( timeout=None,headers=headers) as client:
-        response = await client.post(aim_url, data=payload)
-        data=response.json()
-        img_url = data["data"][0]["url"]
-        await bot.send(event,Image(file=img_url))
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(
+            aim_url,
+            headers=headers,
+            files=file_objects,
+            data=data,
+            timeout=None  # 图像生成较慢，设置长一点的超时
+        )
+        if resp.status_code == 200:
+            res_json = resp.json()
+            img_url = res_json["data"][0]["url"]
+            # 发送结果图片
+            await bot.send(event, [Image(file=img_url)])
+        else:
+            await bot.send(event, f"请求失败 ({resp.status_code}): {resp.text}")
 async def gptimage2_text2img(bot,event,config,prompt):
     user=await get_user(event.user_id)
     permission_need=config.ai_generated_art.config["gptimage2"]["权限要求"]
@@ -49,7 +63,7 @@ async def gptimage2_text2img(bot,event,config,prompt):
         }
 
     async with httpx.AsyncClient( timeout=None,headers=headers) as client:
-        response = await client.post(base_url, data=payload)
+        response = await client.post(base_url, json=payload)
         resp=response.json()
         img_url = resp.json()["data"][0]["url"]
         await bot.send(event,Image(file=img_url))
