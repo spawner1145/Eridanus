850acdc76751840489c4dd30cb52af8c979bb7bb
avilliai <1840094972@qq.com>
2026-05-05 15:16:12 +0800

    上下文长度控制

--- run/mai_reply/service/simple_chat.py
+++ run/mai_reply/service/simple_chat.py
@@ -17,6 +17,6 @@ async def simplified_chat(base_url,prompt,model,api_key,system_prompt):
     }
     if not base_url.endswith("/v1"):
         base_url = base_url + "/v1"
-    async with httpx.AsyncClient(headers=headers) as client:
+    async with httpx.AsyncClient(headers=headers,timeout=None) as client:
         content = await client.post(f"{base_url}/chat/completions",json=data)
         return content.json()["choices"][0]["message"]["content"]
