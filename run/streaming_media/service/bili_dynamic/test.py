7178467b4f17b58db9b19dff7295c1b23b5a330f
manshuo <wlb1270858640@icloud.com>
2026-05-07 13:31:21 +0800

    修bug

--- run/streaming_media/service/bili_dynamic/test.py
+++ run/streaming_media/service/bili_dynamic/test.py
@@ -7,9 +7,9 @@ from bilibili_api.user import create_subscribe_group, set_subscribe_group
 from framework_common.manshuo_draw import *
 
 async def test():
-    await data_delete(7071924)
-    info = await data_init()
-    pprint.pprint(info)
+    #await data_delete(7071924)
+    # info = await data_init()
+    # pprint.pprint(info)
 
     # data_info = await data_init()
     # credential = Credential(sessdata=data_info['cookies']['sessdata'], bili_jct=data_info['cookies']['bili_jct'],
@@ -27,10 +27,9 @@ async def test():
     # info = await bili_followers_live_get()
     # pprint.pprint(info)
 
-    # info = await bili_up_dynamic_monitor(161775300)
-    # pprint.pprint(info)
-    # path = await manshuo_draw(info)
-    # print(path)
+    info = await bili_followers_live_get()
+    pprint.pprint(info)
+
 
 if __name__ == '__main__':
     asyncio.run(test())
