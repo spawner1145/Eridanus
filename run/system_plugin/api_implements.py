160bc79eb4b8d3d03b33b5114656942463860efe
Alpherg <99066610+avilliai@users.noreply.github.com>
2026-05-17 18:16:20 +0800

    Update api_implements.py

--- run/system_plugin/api_implements.py
+++ run/system_plugin/api_implements.py
@@ -40,6 +40,7 @@ def main(bot, config):
             remark = event.pure_text.split("改备注")[1].strip()
             await bot.set_friend_remark(event.user_id, remark)
         if event.pure_text=="罐头笑声":
+            bot.logger.info("正在发送罐头笑声")
             await bot.send(event,Record(file="https://ma.vjshi.com/sound/2025-11-20/92130_VJshi_8dff6e7b1c7e4cdb930593f22039bd4f.mp3"))
 
     @bot.on(GroupBanNoticeEvent)
